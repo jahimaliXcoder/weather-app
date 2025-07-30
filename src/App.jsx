9828dbe59c87293170a2b55917f7e0825b580e35
JAHIM ALI <praveshmehta@JAHIMs-MacBook-Air.local>
2025-07-30 21:15:34 +0530

    all

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className=' text-emerald-400'>Hello World</h1>
+     <p className='text-gray-700'>This is a simple React App using Tailwind CSS.</p>
+    </>
+  )
+}
+
+export default App
